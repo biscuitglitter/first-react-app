685047618ccc626c08b2a4a83e6fb72a1f5ff873
miuhuru <zermoun.yasmine@gmail.com>
2022-02-28 23:34:42 +0100

    app should render an input field and a submit button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+        task: "",
+        array: []
+      }
+  }
+
+  handleInputChange = event => {
+      this.setState( {
+          task: event.target.value
+      })
+  }
+
+  handleSubmit = event => {
+      event.preventDefault()
+  }
+
+  pushToArray = () => {
+    this.state.array.push(this.state.task)
+    console.log(`${this.state.array}`)
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <form onSubmit={this.handleSubmit}>
+              <div>
+                  <label>task</label>
+                  <input type="text" value={this.state.task} onChange={this.handleInputChange} />
+              </div>
+              <button type="submit" onClick={this.pushToArray}>Submit</button>
+        </form>
+        </div>
+          )       
+  }
 }
-
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,16 @@
+import React from "react";
+ 
+class Overview extends React.Component {
+    constructor(props) {
+        super(props)
+
+      
+    }
+
+ 
+    render() {
+        return()       
+    }
+  }
+
+export default Overview
