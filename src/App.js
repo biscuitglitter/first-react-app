cd82cb2f96bb4e88bc1fa7433a87984bccc9c652
miuhuru <zermoun.yasmine@gmail.com>
2022-03-01 19:19:31 +0100

    overview component takes array tasks data and renders each task

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import './App.css';
 import React from "react";
+import Overview from './components/Overview';
 
 class App extends React.Component {
   constructor(props) {
-      super(props)
+    super(props)
 
-      this.state = {
-        task: "",
-        array: []
-      }
+    this.state = {
+      task: "",
+      array: []
+    }
   }
 
   handleInputChange = event => {
-      this.setState( {
-          task: event.target.value
-      })
+    this.setState({
+      task: event.target.value
+    })
   }
 
   handleSubmit = event => {
-      event.preventDefault()
+    event.preventDefault()
+    this.setState({
+      task: ""
+    });
   }
 
   pushToArray = () => {
+    if (this.state.task === "") return
     this.state.array.push(this.state.task)
-    console.log(`${this.state.array}`)
   }
 
   render() {
     return (
       <div className="App">
-          <form onSubmit={this.handleSubmit}>
-              <div>
-                  <label>task</label>
-                  <input type="text" value={this.state.task} onChange={this.handleInputChange} />
-              </div>
-              <button type="submit" onClick={this.pushToArray}>Submit</button>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>task</label>
+            <input type="text" value={this.state.task} onChange={this.handleInputChange} />
+          </div>
+          <button type="submit" onClick={this.pushToArray}>Submit</button>
         </form>
-        </div>
-          )       
+        <Overview dataFromParent={this.state.array} />
+      </div>
+    )
   }
 }
 export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,16 +1,18 @@
 import React from "react";
- 
+
 class Overview extends React.Component {
     constructor(props) {
         super(props)
 
-      
-    }
-
- 
+    } 
     render() {
-        return()       
-    }
+        return (
+            <div className="arrayList"> {this.props.dataFromParent.map((item) => (
+                <li key={ Math.random().toString(36) }>{item}</li>
+              ))}
+             </div> 
+        )       
+    }   
   }
-
 export default Overview
+
